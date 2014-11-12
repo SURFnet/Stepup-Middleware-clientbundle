e4e16179f944c22438e655f59a5f17d47ff39ff5
Reinier Kip <reinier@ibuildings.nl>
2014-11-12 16:34:35 +0100

    Allow consumer to list identity's second factors.

---
+++ src/Identity/Service/SecondFactorService.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClientBundle\Identity\Service;
+
+use Surfnet\StepupMiddlewareClient\Exception\AccessDeniedToResourceException;
+use Surfnet\StepupMiddlewareClient\Exception\MalformedResponseException;
+use Surfnet\StepupMiddlewareClient\Exception\ResourceReadException;
+use Surfnet\StepupMiddlewareClient\Identity\Service\SecondFactorService as LibrarySecondFactorService;
+use Surfnet\StepupMiddlewareClientBundle\Exception\InvalidResponseException;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\Identity;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\SecondFactor;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class SecondFactorService
+{
+    /**
+     * @var LibrarySecondFactorService
+     */
+    private $service;
+
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     * @param LibrarySecondFactorService $service
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(LibrarySecondFactorService $service, ValidatorInterface $validator)
+    {
+        $this->service = $service;
+        $this->validator = $validator;
+    }
+
+    /**
+     * @param string $identityId
+     * @return SecondFactor[]
+     * @throws AccessDeniedToResourceException When the consumer isn't authorised to access given resource.
+     * @throws InvalidResponseException When the API responded with invalid data.
+     * @throws ResourceReadException When the API doesn't respond with the resource.
+     * @throws MalformedResponseException When the API doesn't respond with a proper response.
+     */
+    public function findByIdentity($identityId)
+    {
+        $data = $this->service->findByIdentity($identityId);
+
+        if ($data === null) {
+            return null;
+        }
+
+        $secondFactors = array_map('Surfnet\StepupMiddlewareClientBundle\Identity\Dto\SecondFactor::fromData', $data);
+
+        $violations = $this->validator->validate($secondFactors);
+
+        if (count($violations) > 0) {
+            throw InvalidResponseException::withViolations(
+                "One or more second factors retrieved from the Middleware were invalid",
+                $violations
+            );
+        }
+
+        return $secondFactors;
+    }
+}
---
+++ src/Tests/Identity/Service/SecondFactorServiceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClient\Tests\Identity\Service;
+
+use Mockery as m;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\SecondFactor;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Service\SecondFactorService;
+
+class SecondFactorServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItFindsSecondFactorsByIdentity()
+    {
+        $identityId = 'a';
+
+        $secondFactorData = [
+            [
+                "id" => "769a6649-b3e8-4dd4-8715-2941f947a016",
+                "type" => "yubikey",
+                "second_factor_id" => "ccccccbtbhnh"
+            ]
+        ];
+        $libraryService = m::mock('Surfnet\StepupMiddlewareClient\Identity\Service\SecondFactorService')
+            ->shouldReceive('findByIdentity')->with($identityId)->once()->andReturn($secondFactorData)
+            ->getMock();
+        $violations = m::mock('Symfony\Component\Validator\ConstraintViolationListInterface')
+            ->shouldReceive('count')->with()->once()->andReturn(0)
+            ->getMock();
+        $validator = m::mock('Symfony\Component\Validator\Validator\ValidatorInterface')
+            ->shouldReceive('validate')->once()->andReturn($violations)
+            ->getMock();
+
+        $service = new SecondFactorService($libraryService, $validator);
+        $secondFactors = $service->findByIdentity($identityId);
+
+        /** @var SecondFactor[] $expectedSecondFactors */
+        $expectedSecondFactors = [new SecondFactor()];
+        $expectedSecondFactors[0]->id = $secondFactorData[0]['id'];
+        $expectedSecondFactors[0]->type = $secondFactorData[0]['type'];
+        $expectedSecondFactors[0]->secondFactorId = $secondFactorData[0]['second_factor_id'];
+
+        $this->assertEquals($expectedSecondFactors, $secondFactors);
+    }
+}
