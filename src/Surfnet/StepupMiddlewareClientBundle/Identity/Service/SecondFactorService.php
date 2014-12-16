c9c07187160f3e02d0d7b77e373545e3ef0b40c9
Reinier Kip <reinier@ibuildings.nl>
2014-12-16 10:48:55 +0100

    Add ability to fetch one second factor.

--- src/Surfnet/StepupMiddlewareClientBundle/Identity/Service/SecondFactorService.php
+++ src/Surfnet/StepupMiddlewareClientBundle/Identity/Service/SecondFactorService.php
@@ -26,8 +26,11 @@ use Surfnet\StepupMiddlewareClient\Identity\Dto\VerifiedSecondFactorSearchQuery;
 use Surfnet\StepupMiddlewareClient\Identity\Dto\VettedSecondFactorSearchQuery;
 use Surfnet\StepupMiddlewareClient\Identity\Service\SecondFactorService as LibrarySecondFactorService;
 use Surfnet\StepupMiddlewareClientBundle\Exception\InvalidResponseException;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\UnverifiedSecondFactor;
 use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\UnverifiedSecondFactorCollection;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\VerifiedSecondFactor;
 use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\VerifiedSecondFactorCollection;
+use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\VettedSecondFactor;
 use Surfnet\StepupMiddlewareClientBundle\Identity\Dto\VettedSecondFactorCollection;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -54,6 +57,93 @@ class SecondFactorService
     {
         $this->service = $service;
         $this->validator = $validator;
+    }
+
+    /**
+     * @param string $secondFactorId
+     * @return UnverifiedSecondFactor|null
+     * @throws AccessDeniedToResourceException When the consumer isn't authorised to access given resource.
+     * @throws InvalidResponseException When the API responded with invalid data.
+     * @throws ResourceReadException When the API doesn't respond with the resource.
+     * @throws MalformedResponseException When the API doesn't respond with a proper response.
+     */
+    public function getUnverified($secondFactorId)
+    {
+        $data = $this->service->getUnverified($secondFactorId);
+
+        if ($data === null) {
+            return null;
+        }
+
+        $secondFactor = UnverifiedSecondFactor::fromData($data);
+        $violations = $this->validator->validate($secondFactor);
+
+        if (count($violations) > 0) {
+            throw InvalidResponseException::withViolations(
+                "Unverified second factor retrieved from the Middleware was invalid",
+                $violations
+            );
+        }
+
+        return $secondFactor;
+    }
+
+    /**
+     * @param string $secondFactorId
+     * @return VerifiedSecondFactor|null
+     * @throws AccessDeniedToResourceException When the consumer isn't authorised to access given resource.
+     * @throws InvalidResponseException When the API responded with invalid data.
+     * @throws ResourceReadException When the API doesn't respond with the resource.
+     * @throws MalformedResponseException When the API doesn't respond with a proper response.
+     */
+    public function getVerified($secondFactorId)
+    {
+        $data = $this->service->getVerified($secondFactorId);
+
+        if ($data === null) {
+            return null;
+        }
+
+        $secondFactor = VerifiedSecondFactor::fromData($data);
+        $violations = $this->validator->validate($secondFactor);
+
+        if (count($violations) > 0) {
+            throw InvalidResponseException::withViolations(
+                "Verified second factor retrieved from the Middleware was invalid",
+                $violations
+            );
+        }
+
+        return $secondFactor;
+    }
+
+    /**
+     * @param string $secondFactorId
+     * @return VettedSecondFactor|null
+     * @throws AccessDeniedToResourceException When the consumer isn't authorised to access given resource.
+     * @throws InvalidResponseException When the API responded with invalid data.
+     * @throws ResourceReadException When the API doesn't respond with the resource.
+     * @throws MalformedResponseException When the API doesn't respond with a proper response.
+     */
+    public function getVetted($secondFactorId)
+    {
+        $data = $this->service->getVetted($secondFactorId);
+
+        if ($data === null) {
+            return null;
+        }
+
+        $secondFactor = VettedSecondFactor::fromData($data);
+        $violations = $this->validator->validate($secondFactor);
+
+        if (count($violations) > 0) {
+            throw InvalidResponseException::withViolations(
+                "Vetted second factor retrieved from the Middleware was invalid",
+                $violations
+            );
+        }
+
+        return $secondFactor;
     }
 
     /**
