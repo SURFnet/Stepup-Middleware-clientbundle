4e34c3d938d87f394d67467b005c9f27fa086ff3
Reinier Kip <reinier@ibuildings.nl>
2014-12-17 13:22:28 +0100

    Correct name ID identity search parameter casing.

--- src/Surfnet/StepupMiddlewareClient/Identity/Dto/IdentitySearchQuery.php
+++ src/Surfnet/StepupMiddlewareClient/Identity/Dto/IdentitySearchQuery.php
@@ -118,7 +118,7 @@ class IdentitySearchQuery implements HttpQuery
         }
 
         if ($this->nameId) {
-            $fields['nameId'] = $this->nameId;
+            $fields['NameId'] = $this->nameId;
         }
 
         return '?' . http_build_query($fields);
