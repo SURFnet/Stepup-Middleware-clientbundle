d85c7f7327d174c658b7a636cc0e1dd0e6848447
Michiel Kodde <mkodde@ibuildings.nl>
2019-01-22 16:28:29 +0100

    Consume the profile endpoint
    The /profile endpoint on Middleware API is now supported by the
    Stepup Middleware client bundle. The ProfileSearchQuery must be used
    to retrieve the profile data.

---
+++ src/Surfnet/StepupMiddlewareClient/Identity/Service/ProfileService.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClient\Identity\Service;
+
+use Surfnet\StepupMiddlewareClient\Exception\AccessDeniedToResourceException;
+use Surfnet\StepupMiddlewareClient\Exception\MalformedResponseException;
+use Surfnet\StepupMiddlewareClient\Exception\ResourceReadException;
+use Surfnet\StepupMiddlewareClient\Identity\Dto\ProfileSearchQuery;
+use Surfnet\StepupMiddlewareClient\Service\ApiService;
+
+class ProfileService
+{
+    /**
+     * @var ApiService
+     */
+    private $apiService;
+
+    /**
+     * @param ApiService $apiService
+     */
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    /**
+     * @param ProfileSearchQuery $query
+     * @return null|array
+     * @throws AccessDeniedToResourceException When the consumer isn't authorised to access given resource.
+     * @throws ResourceReadException When the server doesn't respond with the resource.
+     * @throws MalformedResponseException When the server doesn't respond with (well-formed) JSON.
+     */
+    public function get(ProfileSearchQuery $query)
+    {
+        return $this->apiService->read('profile/%s', [$query->getIdentityId()], $query);
+    }
+}
---
+++ src/Surfnet/StepupMiddlewareClientBundle/Identity/Dto/Profile.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClientBundle\Identity\Dto;
+
+use Surfnet\StepupMiddlewareClientBundle\Dto\Dto;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Profile implements Dto
+{
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.id.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.id.must_be_string")
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.name_id.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.name_id.must_be_string")
+     * @var string
+     */
+    public $nameId;
+
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.institution.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.institution.must_be_string")
+     * @var string
+     */
+    public $institution;
+
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.email.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.email.must_be_string")
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.common_name.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.common_name.must_be_string")
+     * @var string
+     */
+    public $commonName;
+
+    /**
+     * @Assert\NotBlank(message="middleware_client.dto.identity.preferred_locale.must_not_be_blank")
+     * @Assert\Type(type="string", message="middleware_client.dto.identity.preferred_locale.must_be_string")
+     * @var string
+     */
+    public $preferredLocale;
+
+    /**
+     * @var bool
+     */
+    public $isSraa;
+
+    /**
+     * @var array
+     */
+    public $authorizations;
+
+    public static function fromData(array $data)
+    {
+        $identity = new self();
+        $identity->id = $data['id'];
+        $identity->nameId = $data['name_id'];
+        $identity->institution = $data['institution'];
+        $identity->email = $data['email'];
+        $identity->commonName = $data['common_name'];
+        $identity->preferredLocale = $data['preferred_locale'];
+        $identity->authorizations = $data['authorizations'];
+        $identity->isSraa = $data['is_sraa'];
+
+        return $identity;
+    }
+}
